86bafbefc43c4160b77c36a97dff9a9c84f34fc9
CassieAin <roksolana.d22@gmail.com>
2017-11-12 15:49:52 +0200

    ManytoMany entities added

--- src/main/scala/models/Film.scala
+++ src/main/scala/models/Film.scala
@@ -20,4 +20,41 @@ class FilmTable(tag: Tag) extends Table[Film](tag, "film") {
   def * = (id, title, duration, directorId, rating)  <> ((Film.apply _).tupled, Film.unapply)
 }
 
+case class FilmToGenre(
+  id: Option[Long],
+  filmId: Long,
+  genreId: Long
+)
 
+class FilmToGenreTable(tag: Tag) extends Table[FilmToGenre](tag, "film_to_genre") {
+  val id = column[Option[Long]]("id", O.PrimaryKey)
+  val filmId = column[Long]("film_id")
+  val genreId = column[Long]("genre_id")
+  def * = (id, filmId, genreId)  <> ((FilmToGenre.apply _).tupled, FilmToGenre.unapply)
+}
+
+case class FilmToCast(
+  id: Option[Long],
+   filmId: Long,
+   staffId: Long
+   )
+
+class FilmToCastTable(tag: Tag) extends Table[FilmToCast](tag, "film_to_cast") {
+  val id = column[Option[Long]]("id", O.PrimaryKey)
+  val filmId = column[Long]("film_id")
+  val staffId = column[Long]("staff_id")
+  def * = (id, filmId, staffId)  <> ((FilmToCast.apply _).tupled, FilmToCast.unapply)
+}
+
+case class FilmToCountry(
+  id: Option[Long],
+  filmId: Long,
+  countryId: Long
+  )
+
+class FilmToCountryTable(tag: Tag) extends Table[FilmToCountry](tag, "film_to_country") {
+  val id = column[Option[Long]]("id", O.PrimaryKey)
+  val filmId = column[Long]("film_id")
+  val countryId = column[Long]("country_id")
+  def * = (id, filmId, countryId)  <> ((FilmToCountry.apply _).tupled, FilmToCountry.unapply)
+}
--- src/main/scala/models/package.scala
+++ src/main/scala/models/package.scala
@@ -1,10 +1,11 @@
 
+import slick.jdbc.JdbcType
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.duration._
 
 package object models {
 
-  implicit val durationToLongMapper =
+  implicit val durationToLongMapper: JdbcType[Duration] =
     MappedColumnType.base[Duration, Long](_.toSeconds,_.seconds)
 }
